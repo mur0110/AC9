b9ce16883523dd7c10a2ebdf5bb26e6fbf09d26f
mur0110 <aqxiy@proton.me>
2024-11-10 19:12:39 -0600

    Update runnerScript2.js

--- runnerScript2.js
+++ runnerScript2.js
@@ -10,8 +10,11 @@ function rngBin(){
 function rngTrio(){
     return Math.floor(Math.random()*3);
 }
+function rngQuat(){
+    return Math.floor(Math.random()*4);
+}
 
-const songPath = 'songs/starlight.mp3';
+const songPath = 'starlight.mp3';
 
 
 const volArr=[3, 5, 7, 7, 5, 3];
@@ -103,7 +106,7 @@ function runner(){
           randomForVolVal = multVolArr[randomsForVol[randomsForVolIter]];
           randomsForVolIter++;
           if (rngBin()===0){
-            if (rngBin()===0){
+            if (rngQuat()===0){
                 modInterDuration();
             }
           }
@@ -126,7 +129,7 @@ function runner(){
           randomForSpeedVal = multSpeedArr[randomsForSpeed[randomsForSpeedIter]];
           randomsForSpeedIter++;
           if (rngBin()===0){
-            if (rngBin()===0){
+            if (rngQuat()===0){
                 modInterDuration();
             }
           }
