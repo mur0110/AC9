f6b6dc15ee0e534677250a67a7baf545cbd40671
mur0110 <aqxiy@proton.me>
2024-11-14 15:45:47 -0600

    Update runnerScript4.js

--- runnerScript4.js
+++ runnerScript4.js
@@ -2,7 +2,7 @@ let interDuration = .15;
 
 function modInterDuration(){
     const possibleDeviations=[.13, .17, .19, .23, .29];
-    interDuration = possibleDeviations[Math.floor(Math.random()*4)];
+    interDuration = possibleDeviations[Math.floor(Math.random()*5)];
 }
 function rngBin(){
     return Math.floor(Math.random()*2);
