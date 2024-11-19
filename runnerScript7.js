1e25b61ceb7556ac1fb510da13afb64335af8cb0
mur0110 <aqxiy@proton.me>
2024-11-18 21:03:25 -0600

    Update runnerScript7.js

--- runnerScript7.js
+++ runnerScript7.js
@@ -14,7 +14,7 @@ function rngQuat(){
     return Math.floor(Math.random()*4);
 }
 
-const songPath = 'songs/intheairton.mp3';
+const songPath = 'songs/sweetchild.mp3';
 
 
 const volArr=[3, 5, 7, 7, 5, 3];
