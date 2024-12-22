9d369b1948e5a8cc103e9506f77ff071ac812c44
mur0110 <aqxiy@proton.me>
2024-12-22 10:11:20 -0600

    Update runnerScriptEcs.js

--- runnerScriptEcs.js
+++ runnerScriptEcs.js
@@ -686,20 +686,20 @@ function runner(){
 
               if (!rngOutput){
                 if (rngBin() === 0){
-                    rngVS1=dp3*(.595/1.695);
+                    rngVS1=dp3*(1.19/1.695);
                 }
                 else {
-                    rngVS1=dp4*(.595/1.35845);
+                    rngVS1=dp4*(1.19/1.35845);
                 }
               }
 
               
               else {
                 if (rngBin() === 0){
-                    rngVS1=Math.sqrt(dp3)*(.595/1.302);
+                    rngVS1=Math.sqrt(dp3)*(1.19/1.302);
                 }
                 else{
-                    rngVS1=Math.sqrt(dp4)*(.595/1.166);
+                    rngVS1=Math.sqrt(dp4)*(1.19/1.166);
                 }
               }
               
