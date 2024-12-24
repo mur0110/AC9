f0e2eec430b0230460a2a35fc8004ae7285069fa
mur0110 <aqxiy@proton.me>
2024-12-23 18:30:56 -0600

    Update runnerScriptShineal.js

--- runnerScriptShineal.js
+++ runnerScriptShineal.js
@@ -5,8 +5,8 @@ let repeatV=0;
 let repeatS=0;
 
 function modInterDuration(){
-    let possibleDeviations=[.11, .13, .17, .19, .23, .29, .31, .37, .41, .43, .47, .53, .57];
-    interDuration = possibleDeviations[Math.floor(Math.random()*13)];
+    let possibleDeviations=[.07, .11, .13, .17, .19, .23, .29, .31, .37, .41, .43, .47, .53, .57];
+    interDuration = possibleDeviations[Math.floor(Math.random()*14)];
 
 }
 function rngBin(){
