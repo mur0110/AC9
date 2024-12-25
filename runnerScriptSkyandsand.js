1a4fb086761f014ce8e413cc88102ecbfbf1f818
mur0110 <aqxiy@proton.me>
2024-12-25 15:34:22 -0600

    Update runnerScriptSkyandsand.js

--- runnerScriptSkyandsand.js
+++ runnerScriptSkyandsand.js
@@ -5,9 +5,9 @@ let repeatV=0;
 let repeatS=0;
 
 function modInterDuration(){
-    let possibleDeviations=[.05, .07, .11, .13, .17, .19, .23, .29, .31, .37, .41, .43, .47, .53, .57, .59, .61, .67, .71, .73, .79, .83, .87, .89, .91, .97];
+    let possibleDeviations=[.05, .07, .11, .13, .17, .19, .23, .29, .31, .37, .41, .43, .47, .53, .57];
     if (rngBin()===0){
-        interDuration = possibleDeviations[Math.floor(Math.random()*26)];
+        interDuration = possibleDeviations[Math.floor(Math.random()*15)];
     }
 }
 function rngBin(){
