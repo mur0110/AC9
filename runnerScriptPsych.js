99dcf97ff0534111bb1189eff7e57be8310a1719
mur0110 <aqxiy@proton.me>
2025-01-02 19:39:32 -0600

    Update runnerScriptPsych.js

--- runnerScriptPsych.js
+++ runnerScriptPsych.js
@@ -55,7 +55,7 @@ function modInterDuration(){
         mVal=17;
     }
 
-    if (rngR()){
+    if (!rngRx()){
         interDuration = mArray[Math.floor(rngs[rngIter]*mVal)];
         rngIter++;
     }
