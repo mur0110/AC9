818d80690623aa9bd09e436a333694c3c0ad64c2
mur0110 <aqxiy@proton.me>
2025-01-08 16:21:18 -0600

    Update runnerScriptChildren.js

--- runnerScriptChildren.js
+++ runnerScriptChildren.js
@@ -1,4 +1,4 @@
-const songPath = 'songs/children.mp3'; //children was first
+const songPath = 'songs/children.mp3'; //thewanted we own the night was first
 
 
 let volumeLog=0;
@@ -378,7 +378,7 @@ function redefineVolInterval(){
     rngIter++;
     let randomInt=0;
 
-    if (!rngRx()){
+    if (rngRx()){
         randomInt=Math.floor(arrayHold[Math.floor(rngs[rngIter]*arrayHold.length)]);
         rngIter++;
     }
@@ -470,7 +470,7 @@ function redefineSpeedInterval(){
     rngIter++;
     let randomInt=0;
 
-    if (!rngRx()){
+    if (rngRx()){
         randomInt=Math.floor(arrayHold[Math.floor(rngs[rngIter]*arrayHold.length)]);
         rngIter++;
     }
