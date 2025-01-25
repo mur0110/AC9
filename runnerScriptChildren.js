2b626a6b11dd53ec811b1182ab6adf47336271d3
mur0110 <aqxiy@proton.me>
2025-01-25 11:42:16 -0600

    Update runnerScriptChildren.js

--- runnerScriptChildren.js
+++ runnerScriptChildren.js
@@ -463,18 +463,23 @@ function redefineVolInterval(){
 
 
 
-        let rngTr=rngQuat();
-        if (rngTr===0){
-            multiplier=(multiplier**(1/2) + multiplier**(1/3) + multiplier**(1/4))/3;
+
+        if (!rngRx()){}
+        else{
+            let rngTr=rngTrio();
+            if (rngTr===0){
+                multiplier=(multiplier**(1/2) + multiplier**(1/3) + multiplier**(1/4))/3;
+            }
+            else if (rngTr===1)
+            {
+                multiplier=multiplier**(5/7);
+            }
+            else if (rngTr===2){
+                multiplier=multiplier**(7/5);
+            }
         }
-        else if (rngTr===1)
-        {
-            multiplier=multiplier**(5/7);
-        }
-        else if (rngTr===2){
-            multiplier=multiplier**(7/5);
-        }
-        else{}
+        
+
 
     
     
@@ -571,7 +576,9 @@ function redefineSpeedInterval(){
         }
     }
 
-    let rngTr=rngQuat();
+    if (!rngRx()){}
+    else{
+        let rngTr=rngTrio();
         if (rngTr===0){
             multiplier=(multiplier**(1/2) + multiplier**(1/3) + multiplier**(1/4))/3;
         }
@@ -582,7 +589,7 @@ function redefineSpeedInterval(){
         else if (rngTr===2){
             multiplier=multiplier**(7/5);
         }
-        else{}
+    }
 
    
     if (!rngRx()){
