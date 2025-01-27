8d17706a0e914b251fa5b021afde3f0ab8c6fee3
mur0110 <aqxiy@proton.me>
2025-01-27 14:05:03 -0600

    Update runnerScriptChildren.js

--- runnerScriptChildren.js
+++ runnerScriptChildren.js
@@ -1068,7 +1068,7 @@ function runner(){
                     
                 }
             }
-            if (rngR()){
+            if (rngRx()){
                 nextVolIntervalIter();
                 redefineVolInterval();
             }
@@ -1243,7 +1243,7 @@ function runner(){
                     
                 }
             }
-            if (rngR()){
+            if (rngRx()){
                 nextSpeedIntervalIter();
                 redefineSpeedInterval();
             }
