280c99e2b2e1fd2ee95079f0492cb2e3e415d434
mur0110 <aqxiy@proton.me>
2025-02-10 16:43:04 -0600

    Update runnerScriptChildren.js

--- runnerScriptChildren.js
+++ runnerScriptChildren.js
@@ -1,4 +1,4 @@
-const songPath = 'ifeel.mp3'; //  ifeelgoodaboutThis was 1st
+const songPath = 'ifeel.mp3'; //  ifeel was 1st
 let duration1=490;
 
 
@@ -1134,16 +1134,16 @@ function runner(){
 
 
         if (rngR()){
-            if (rngRx()){
+            if (rngR()){
                 nextVolIntervalIter();
                 redefineVolInterval();
             }
-            if (rngRx()){
+            if (rngR()){
                 randomsForVolIter++;
             }
         }
         else{
-            if (rngRx()){
+            if (rngR()){
                 nextVolIntervalIter();
                 redefineVolInterval();
                 randomsForVolIter++;
@@ -1323,16 +1323,16 @@ function runner(){
 
 
         if (rngR()){
-            if (rngRx()){
+            if (rngR()){
                 nextSpeedIntervalIter();
                 redefineSpeedInterval();
             }
-            if (rngRx()){
+            if (rngR()){
                 randomsForSpeedIter++;
             }
         }
         else{
-            if (rngRx()){
+            if (rngR()){
                 nextSpeedIntervalIter();
                 redefineSpeedInterval();
                 randomsForSpeedIter++;
