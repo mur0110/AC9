f4e5cbd214cc10c085ac4167a56844436277e45a
mur0110 <aqxiy@proton.me>
2025-02-21 19:28:21 -0600

    Update runnerScriptChildren.js

--- runnerScriptChildren.js
+++ runnerScriptChildren.js
@@ -1,4 +1,4 @@
-const songPath = 'wherei.mp3'; //  summer was 1st
+const songPath = 'atlashands.mp3'; //  summer was 1st
 let duration1=490;
 
 
@@ -385,7 +385,7 @@ function redefineVolInterval(){
     rngIter++;
     let randomInt=0;
 
-    if (!rngRx()){
+    if (rngRx()){
         randomInt=Math.floor(arrayHold[Math.floor(rngs[rngIter]/1000 *arrayHold.length)]);
         rngIter++;
     }
@@ -508,7 +508,7 @@ function redefineSpeedInterval(){
     rngIter++;
     let randomInt=0;
 
-    if (!rngRx()){
+    if (rngRx()){
         randomInt=Math.floor(arrayHold[Math.floor(rngs[rngIter]/1000 *arrayHold.length)]);
         rngIter++;
     }
